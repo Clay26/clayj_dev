4fd5a7dfbfe79010b50d19ef2f2d9cafe759ae32
clay <5711623+Clay26@users.noreply.github.com>
2023-03-22 09:34:22 -0400

    Update webpage

---
+++ clayj_web/src/components/ContentItem.vue
@@ -0,0 +1,54 @@
+<template>
+  <div class="item">
+    <i>
+      <slot name="icon"></slot>
+    </i>
+    <div class="details">
+      <h1>
+        <slot name="heading"></slot>
+      </h1>
+      <slot></slot>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.item {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  padding-right: 30px;
+  padding-left: 30px;
+}
+
+.details {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  max-width: 400px;
+}
+.item:before {
+  content: ' ';
+  border-left: 1px solid var(--color-border);
+  position: absolute;
+  left: 0;
+  bottom: calc(50% + 25px);
+  height: calc(50% - 25px);
+}
+.item:after {
+  content: ' ';
+  border-left: 1px solid var(--color-border);
+  position: absolute;
+  left: 0;
+  top: calc(50% + 25px);
+  height: calc(50% - 25px);
+}
+.item:first-of-type:before {
+  display: none;
+}
+.item:last-of-type:after {
+  display: none;
+}
+
+</style>
---
+++ clayj_web/src/components/WelcomePage.vue
@@ -0,0 +1,46 @@
+<script setup lang="ts">
+import ContentItem from './ContentItem.vue'
+import DocumentationIcon from './icons/IconDocumentation.vue'
+import ToolingIcon from './icons/IconTooling.vue'
+import EcosystemIcon from './icons/IconEcosystem.vue'
+import CommunityIcon from './icons/IconCommunity.vue'
+import SupportIcon from './icons/IconSupport.vue'
+</script>
+
+<template>
+  <div class="items">
+  <ContentItem>
+    <template #icon>
+      <DocumentationIcon />
+    </template>
+    <template #heading>Brand Design</template>
+
+    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Commodo nulla facilisi nullam vehicula ipsum a arcu cursus vitae. Semper viverra nam libero justo laoreet sit.
+  </ContentItem>
+
+  <ContentItem>
+    <template #icon>
+      <ToolingIcon />
+    </template>
+    <template #heading>Seo & Marketing</template>
+
+    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Commodo nulla facilisi nullam vehicula ipsum a arcu cursus vitae. Semper viverra nam libero justo laoreet sit.
+  </ContentItem>
+
+  <ContentItem>
+    <template #icon>
+      <EcosystemIcon />
+    </template>
+    <template #heading>Creative Design</template>
+
+    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Commodo nulla facilisi nullam vehicula ipsum a arcu cursus vitae. Semper viverra nam libero justo laoreet sit.
+  </ContentItem>
+  </div>
+</template>
+
+<style scoped>
+.items {
+  display: flex;
+  justify-content: space-evenly;
+}
+</style>
