50130d8e28e75bb60d4002b4c5c4f51af3dea2cc
Clay Jackson <cljackso@microsoft.com>
2023-03-22 16:32:06 -0400

    Add Welcome Banner

---
+++ clayj_web/src/components/WelcomeBanner.vue
@@ -0,0 +1,44 @@
+<script lang="ts">
+export default {
+  props: {
+    imageUrl: {
+      type: String,
+      required: true,
+    },
+  },
+  mounted() {
+    this.$el.style.backgroundImage = `url(${this.imageUrl})`;
+  },
+};
+</script>
+
+<template>
+  <div class="BackgroundImage">
+    <slot name="backgroundImage"></slot>
+    <div class="bannerText">
+        <slot name="content"></slot>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.background-image {
+  background-position: center;
+  background-repeat: no-repeat;
+  background-size: cover;
+  min-height: 100vh;
+  width: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  background-image: url();
+}
+
+.BackgroundImage {
+  min-height: 50vh;
+  margin: 0px;
+  padding: 0px;
+  border: 0px;
+  width: 100%;
+}
+</style>
--- clayj_web/src/components/WelcomePage.vue
+++ clayj_web/src/components/WelcomePage.vue
@@ -1,44 +1,61 @@
 <script setup lang="ts">
 import ContentItem from './ContentItem.vue'
+import WelcomeBanner from './WelcomeBanner.vue'
 import DocumentationIcon from './icons/IconDocumentation.vue'
 import ToolingIcon from './icons/IconTooling.vue'
 import EcosystemIcon from './icons/IconEcosystem.vue'
 import CommunityIcon from './icons/IconCommunity.vue'
 import SupportIcon from './icons/IconSupport.vue'
+import backgroundImage from "../assets/BannerPhoto.png"
 </script>
 
 <template>
-  <div class="items">
-  <ContentItem>
-    <template #icon>
-      <DocumentationIcon />
-    </template>
-    <template #heading>Brand Design</template>
+  <div class="container">
+    <WelcomeBanner :imageUrl=backgroundImage>
+      <template #content>
+        <h1>Welcome to the ClayJ Web</h1>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Commodo nulla facilisi nullam vehicula ipsum a arcu cursus vitae. Semper viverra nam libero justo laoreet sit.
+        </p>
+      </template>
+    </WelcomeBanner>
+    <div class="items">
+    <ContentItem>
+      <template #icon>
+        <DocumentationIcon />
+      </template>
+      <template #heading>Brand Design</template>
 
-    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Commodo nulla facilisi nullam vehicula ipsum a arcu cursus vitae. Semper viverra nam libero justo laoreet sit.
-  </ContentItem>
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Commodo nulla facilisi nullam vehicula ipsum a arcu cursus vitae. Semper viverra nam libero justo laoreet sit.
+    </ContentItem>
 
-  <ContentItem>
-    <template #icon>
-      <ToolingIcon />
-    </template>
-    <template #heading>Seo & Marketing</template>
+    <ContentItem>
+      <template #icon>
+        <ToolingIcon />
+      </template>
+      <template #heading>Seo & Marketing</template>
 
-    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Commodo nulla facilisi nullam vehicula ipsum a arcu cursus vitae. Semper viverra nam libero justo laoreet sit.
-  </ContentItem>
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Commodo nulla facilisi nullam vehicula ipsum a arcu cursus vitae. Semper viverra nam libero justo laoreet sit.
+    </ContentItem>
 
-  <ContentItem>
-    <template #icon>
-      <EcosystemIcon />
-    </template>
-    <template #heading>Creative Design</template>
+    <ContentItem>
+      <template #icon>
+        <EcosystemIcon />
+      </template>
+      <template #heading>Creative Design</template>
 
-    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Commodo nulla facilisi nullam vehicula ipsum a arcu cursus vitae. Semper viverra nam libero justo laoreet sit.
-  </ContentItem>
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Commodo nulla facilisi nullam vehicula ipsum a arcu cursus vitae. Semper viverra nam libero justo laoreet sit.
+    </ContentItem>
+    </div>
   </div>
 </template>
 
 <style scoped>
+.container {
+  display: flex;
+  flex-direction: column;
+}
+
 .items {
   display: flex;
   justify-content: space-evenly;
