12ee85d65321528529679de2342deb261122dad1
Clay Jackson <cljackso@microsoft.com>
2023-04-05 22:06:10 -0400

    Add azure swa adapter

--- clayj_web/svelte.config.js
+++ clayj_web/svelte.config.js
@@ -1,5 +1,10 @@
 import azureSWA from 'svelte-adapter-azure-swa';
+import adapter from '@sveltejs/adapter-auto';
+
 import { vitePreprocess } from '@sveltejs/kit/vite';
+
+const selectedAdapter = process.env.DEPLOY_ENV === 'azure' ? azureSWA() : adapter();
+
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
@@ -11,7 +16,7 @@ const config = {
 		// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
 		// If your environment is not supported or you settled on a specific environment, switch out the adapter.
 		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
-		adapter: azureSWA()
+		adapter: selectedAdapter
 	}
 };
 
