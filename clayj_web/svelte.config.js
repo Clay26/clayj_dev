a8357cd6b66a0a9cf60e296e4be0ce8cc79909c8
Clay Jackson <cljackso@microsoft.com>
2023-04-05 22:52:12 -0400

    I think this will work....

--- clayj_web/svelte.config.js
+++ clayj_web/svelte.config.js
@@ -1,5 +1,7 @@
 import azureSWA from 'svelte-adapter-azure-swa';
 import adapter from '@sveltejs/adapter-auto';
+import adapterStatic from '@sveltejs/adapter-static';
+
 
 import { vitePreprocess } from '@sveltejs/kit/vite';
 
@@ -17,7 +19,12 @@ const config = {
 		// If your environment is not supported or you settled on a specific environment, switch out the adapter.
 		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
 		// adapter: selectedAdapter
-		adapter: azureSWA()
+		adapter: adapterStatic({
+			// These options are optional; the defaults are shown here
+			pages: 'build', // Directory for generated static pages
+			assets: 'build', // Directory for generated static assets
+			fallback: null, // Fallback HTML file, usually 'index.html' for single page apps
+    }),
 	}
 };
 
