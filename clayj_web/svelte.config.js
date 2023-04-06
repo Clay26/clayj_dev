c3224e1284753022db846dbee1b5d0f403b57822
Clay Jackson <cljackso@microsoft.com>
2023-04-05 23:04:05 -0400

    Clean up config

--- clayj_web/svelte.config.js
+++ clayj_web/svelte.config.js
@@ -1,12 +1,5 @@
-import azureSWA from 'svelte-adapter-azure-swa';
-import adapter from '@sveltejs/adapter-auto';
 import adapterStatic from '@sveltejs/adapter-static';
-
-
 import { vitePreprocess } from '@sveltejs/kit/vite';
-
-// const selectedAdapter = process.env.DEPLOY_ENV === 'azure' ? azureSWA() : adapter();
-
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
@@ -18,7 +11,6 @@ const config = {
 		// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
 		// If your environment is not supported or you settled on a specific environment, switch out the adapter.
 		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
-		// adapter: selectedAdapter
 		adapter: adapterStatic({
 			// These options are optional; the defaults are shown here
 			pages: 'build', // Directory for generated static pages
