4fb80aa280653e39e63da674528af7d59eb4dd5b
Clay Jackson <cljackso@microsoft.com>
2023-04-07 13:48:41 -0400

    Fix headar padding

--- clayj_web/src/components/Header.svelte
+++ clayj_web/src/components/Header.svelte
@@ -3,7 +3,7 @@
     let logo = '/images/robot.png';
 </script>
 
-<div class="flex justify-between px-14 bg-header-green">
+<div class="flex justify-between px-4 bg-header-green">
     <a href="/">
         <img src={logo} alt="Robot Mascot" class="max-h-16 w-auto"/>
     </a>
