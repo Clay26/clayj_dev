e538472ad2c1a7d9eca368fbdd79eb1d5e0daf24
Clay Jackson <cljackso@microsoft.com>
2023-04-10 17:19:54 -0400

    Align footer links to center

--- clayj_web/src/components/Footer.svelte
+++ clayj_web/src/components/Footer.svelte
@@ -8,7 +8,7 @@
     ];
 </script>
 
-<div class="self-end flex justify-between min-w-full px-10 h-10 bg-footer text-white">
+<div class="self-end flex justify-between items-center min-w-full px-10 h-10 bg-footer text-white">
     {#each contacts as contact}
     <NavItem {...contact} />
     {/each}
