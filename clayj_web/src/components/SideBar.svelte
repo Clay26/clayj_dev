4c1076ec5cab223c7f6da5bd3fb388b9388ca839
Clay Jackson <cljackso@microsoft.com>
2023-04-10 17:12:33 -0400

    Update close button name

--- clayj_web/src/components/SideBar.svelte
+++ clayj_web/src/components/SideBar.svelte
@@ -9,9 +9,9 @@
     ];
 </script>
 
-<div class="flex flex-col justify-start items-center gap-10 font-bold">
-    <button on:click={toggleSideBar} class="self-start text-banneraccent hover:text-banneraccentlt mx-5 my-3">
-        X
+<div class="flex flex-col justify-start items-start px-4 gap-5 font-bold text-2xl">
+    <button on:click={toggleSideBar} class="self-start text-banneraccent hover:text-banneraccentlt text-3xl my-3">
+        Menu
     </button>
     {#each pages as page}
         <div>
