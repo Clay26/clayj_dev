9444f358cb0e6cf967cb60b7b44503ac9446be99
Clay Jackson <cljackso@microsoft.com>
2023-04-10 22:06:51 -0400

    Update footer to copywright plus date

--- clayj_web/src/components/Footer.svelte
+++ clayj_web/src/components/Footer.svelte
@@ -1,15 +1,3 @@
-<script lang="ts">
-    import NavItem from "./NavItem.svelte";
-
-    const contacts = [
-        { href:"https://github.com/Clay26", text:"Clay26 - GitHub" },
-        { href:"https://twitter.com", text:"Twitter" },
-        { href:"https://github.com/Clay26/clayj_dev", text:"clayj_dev" }
-    ];
-</script>
-
-<div class="self-end flex justify-between items-center min-w-full px-10 h-10 bg-footer text-white">
-    {#each contacts as contact}
-    <NavItem {...contact} />
-    {/each}
-</div>
+<span class="self-end flex justify-between items-center min-w-full px-10 h-10 bg-footer text-white">
+    &#169 {new Date().getFullYear()} Clay Jackson
+</span>
