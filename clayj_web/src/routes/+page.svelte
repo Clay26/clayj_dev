fb9e4c6aa0dd6c39c618bd425e0cf3b52f90dd3a
clay <5711623+Clay26@users.noreply.github.com>
2023-04-12 16:42:44 -0400

    Add front page content

--- clayj_web/src/routes/+page.svelte
+++ clayj_web/src/routes/+page.svelte
@@ -12,14 +12,14 @@
 			<p slot="content">Within Microsoft's Identity organization, even a minor decrease in availability can significantly impact our numerous Fortune 500 clients globally. As such, we strive to achieve a 99.999% availability rate. I have extensive experience operating within high-availability environments, skillfully designing migrations, mitigations, and modifications that contribute to achieving this ambitious availability target. I have sharpened my skills in live site troubleshooting and incident response. Utilizing tools such as Data Explorer, Postman, and Wireshark, I have successfully navigated numerous challenging live site problems to ensure the smooth recovery of AAD services.</p>
 		</ContentItem>
 		<ContentItem>
-			<span slot="heading">Large-Scale Service Expertise</span>
+			<span slot="heading">Large-Scale Service Background</span>
 
 			<p slot="content">As a software engineer on the Azure Active Directory (AAD) team, I have sharpened my skills in managing and maintaining some of Microsoft's largest and most essential services. My primary responsibility was as an engineer on the AAD Gateway team, a Level 7 reverse proxy that functions as an API Gateway for most AAD services. Handling a staggering 200 billion requests daily, the AAD Gateway demanded extensive large-scale service expertise. In this high-volume environment, I have gained invaluable experience designing scalable features, addressing edge cases, and ensuring optimal performance.</p>
 		</ContentItem>
 		<ContentItem>
-			<span slot="heading">High-Volume API Gateway Management</span>
+			<span slot="heading">Build Out Expertise</span>
 
-			<p slot="content">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Commodo nulla facilisi nullam vehicula ipsum a arcu cursus vitae. Semper viverra nam libero justo laoreet sit.</p>
+			<p slot="content">I have successfully expanded numerous regions for complex services such as AAD Gateway, ESTS (the token issuance service for AAD), and Azure MFA. These build-outs demanded extensive troubleshooting in the codebase to identify and resolve missing dependencies, including Azure resources, configurations, role assignments, and more. Throughout the build-out process, I have continuously refined my approach and implemented automation where possible to streamline the expansion of new regions or environments. As a result, each subsequent build-out has become increasingly efficient and expeditious, ensuring a smooth deployment process with minimal setbacks.</p>
 		</ContentItem>
 	</div>
 </div>
