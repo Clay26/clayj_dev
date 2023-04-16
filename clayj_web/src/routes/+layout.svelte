4d7e7336fa1c1bf55fe7d4b9cb2b841787c17100
Clay26 <5711623+Clay26@users.noreply.github.com>
2023-04-16 16:56:22 -0400

    Move to autosubscriber (#11)

--- clayj_web/src/routes/+layout.svelte
+++ clayj_web/src/routes/+layout.svelte
@@ -4,12 +4,6 @@
 	import Footer from "../components/Footer.svelte";
 	import Header from "../components/Header.svelte";
 	import SideBar from "../components/SideBar.svelte";
-
-    let isSideBarOpen = false;
-
-    isOpened.subscribe(value => {
-        isSideBarOpen = value;
-    });
 </script>
 
 <div class="flex flex-col min-h-screen w-full">
@@ -17,6 +11,6 @@
     <slot />
     <Footer />
 </div>
-<div class="bg-bannerbg fixed top-0 right-0 h-full w-full md:w-56 lg:w-72 transistion-all ease-in-out duration-500 {isSideBarOpen ? '' : 'translate-x-full'}">
+<div class="bg-bannerbg fixed top-0 right-0 h-full w-full md:w-56 lg:w-72 transistion-all ease-in-out duration-500 {$isOpened ? '' : 'translate-x-full'}">
     <SideBar />
 </div>
