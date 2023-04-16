6119022dd408f5831b167132f808bb9d41206775
Clay26 <5711623+Clay26@users.noreply.github.com>
2023-04-16 18:08:44 -0400

    Switch to svelte animations for sidebar (#12)

--- clayj_web/src/routes/+layout.svelte
+++ clayj_web/src/routes/+layout.svelte
@@ -1,16 +1,39 @@
 <script lang="ts">
-    import "../app.css";
-    import { isOpened } from "../stores/sidebar.js";
-	import Footer from "../components/Footer.svelte";
-	import Header from "../components/Header.svelte";
-	import SideBar from "../components/SideBar.svelte";
+	import '../app.css';
+	import { onMount } from 'svelte';
+	import { fly } from 'svelte/transition';
+	import { isOpened } from '../stores/sidebar.js';
+	import Footer from '../components/Footer.svelte';
+	import Header from '../components/Header.svelte';
+	import SideBar from '../components/SideBar.svelte';
+
+	let sidebarWidth:number;
+    
+    function calcSidebarWidth(windowWidth: number): number {
+		// Small screen case: Sidebar takes up whole screen
+		// Medium and Large screens: Sidebar takes up 25% of screen
+		return windowWidth < 768 ? windowWidth : windowWidth * 0.25;
+    }
+
+	onMount(() => {
+		sidebarWidth = calcSidebarWidth(window.innerWidth);
+        window.addEventListener('resize', () => {
+            sidebarWidth = calcSidebarWidth(window.innerWidth);
+        });
+	});
 </script>
 
 <div class="flex flex-col min-h-screen w-full">
-    <Header />
-    <slot />
-    <Footer />
+	<Header />
+	<slot />
+	<Footer />
 </div>
-<div class="bg-bannerbg fixed top-0 right-0 h-full w-full md:w-56 lg:w-72 transistion-all ease-in-out duration-500 {$isOpened ? '' : 'translate-x-full'}">
-    <SideBar />
-</div>
+{#if $isOpened}
+	<div
+		in:fly={{ x: sidebarWidth, duration: 500 }}
+		out:fly={{ x: sidebarWidth, duration: 500 }}
+		class="bg-bannerbg fixed top-0 right-0 h-full w-full md:w-[25%]"
+	>
+		<SideBar />
+	</div>
+{/if}
