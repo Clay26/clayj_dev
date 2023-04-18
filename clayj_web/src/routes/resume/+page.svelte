f084f4be413ee6157376b95cf824b41d9e08b7a9
clay <5711623+Clay26@users.noreply.github.com>
2023-04-18 10:52:25 -0400

    Add job duties

--- clayj_web/src/routes/resume/+page.svelte
+++ clayj_web/src/routes/resume/+page.svelte
@@ -9,15 +9,12 @@
         timeframe:"03/2020 - Current",
         href:"https://www.microsoft.com",
         duties: [
-            "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
-            "Donec et odio pellentesque diam volutpat commodo sed.",
-            "Porta non pulvinar neque laoreet suspendisse interdum.",
-            "Rutrum quisque non tellus orci ac.",
-            "Sed vulputate odio ut enim blandit volutpat.",
-            "Consectetur adipiscing elit ut aliquam.",
-            "Ipsum a arcu cursus vitae congue mauris rhoncus aenean vel.",
-            "Arcu dui vivamus arcu felis bibendum ut."
-
+            "Software Engineer on AAD Gateway, a reverse proxy service within the Identity organization that serves over 200 billion daily requests.",
+            "Led the development of a feature to expose API rate limiting for customers to monitor and manage their usage of B2C login.",
+            "Designed and implemented the architecture for a new internal tool used to read the Azure Active Directory states. The tool uses a React web front end with a C\# ASP.NET Core backend.",
+            "Leveraged Postman, PowerShell, and Data Explorer to provide live site support to resolve issues and ensure high SLA for AAD Gateway, MS Graph, ESTS, and other AAD services.",
+            "Built out several Identity services, such as Azure MFA, into new regions to extend service availability and enable greater security for customers.",
+            "Conducted performance evaluations and optimizations of various logging features to ensure AAD Gateway meets COGS and reliability requirements."
         ]
         },
         {
@@ -26,12 +23,10 @@
         timeframe:"07/2018 - 03/2020",
         href:"https://www.northropgrumman.com",
         duties: [
-            "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
-            "Tristique sollicitudin nibh sit amet commodo nulla facilisi nullam vehicula.",
-            "Rhoncus urna neque viverra justo nec.",
-            "Nunc sed id semper risus in.",
-            "Magna fermentum iaculis eu non diam phasellus vestibulum lorem."
-
+            "Collaborated with radar domain experts to extend ARSIM in C++, a radar simulation framework, to include new radar modes.",
+            "Developed plugins in C++ for AFSIM, a military mission simulation framework, to develop new scenarios.",
+            "Implemented a TCP/IP interface to link together two radar simulated systems.",
+            "Developed algorithms to allocate radar resources intelligently based on radar data."
         ]
         },
     ];
