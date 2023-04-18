e4b78122cdc3c79507a63593e99cbec8d7e427c4
clay <5711623+Clay26@users.noreply.github.com>
2023-04-18 12:57:51 -0400

    Add github link to footer

--- clayj_web/src/components/Footer.svelte
+++ clayj_web/src/components/Footer.svelte
@@ -1,3 +1,12 @@
+<script>
+    import github from "$lib/images/github-mark-white.svg";
+</script>
+
 <span class="self-end flex justify-between items-center min-w-full px-10 h-10 bg-footer text-white">
     &#169 {new Date().getFullYear()} Clay Jackson
+    <div class="flex justify-center w-7">
+        <a href="https://github.com/clay26">  
+            <img src={github} alt="Github"/>
+        </a>
+    </div>
 </span>
