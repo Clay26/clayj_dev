7dd123bd60268a63f4a0224578720e55a99e01d3
Clay26 <5711623+Clay26@users.noreply.github.com>
2023-04-18 10:31:47 -0400

    Close sidebar on link click (#14)

--- clayj_web/src/components/NavItem.svelte
+++ clayj_web/src/components/NavItem.svelte
@@ -1,8 +1,10 @@
 <script lang="ts">
     export let href = "https://www.clayjackson.dev";
     export let text = "Home";
+
+    import { toggleSideBar } from "../lib/utils.ts";
 </script>
 
-<a {href} class="text-center text-banneraccent hover:text-banneraccentlt hover:underline">
+<a {href} on:click={toggleSideBar} class="text-center text-banneraccent hover:text-banneraccentlt hover:underline">
     {text}
 </a>
--- clayj_web/src/routes/now/+page.svelte
+++ clayj_web/src/routes/now/+page.svelte
@@ -1,11 +1,5 @@
 <script lang="ts">
-	import { onMount } from "svelte";
-	import { isOpened } from "../../stores/sidebar.ts";
 	import Header from "../../components/Header.svelte";
-
-    onMount(() => {
-        isOpened.set(false);
-    });
 </script>
 
 <div class="grow">
